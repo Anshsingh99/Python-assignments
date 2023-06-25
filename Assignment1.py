4649999854e3fa35e0c6c03e1752935075a518a7
Anshsingh99 <137695101+Anshsingh99@users.noreply.github.com>
2023-06-25 18:12:09 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,8 @@
+num=int(input("Enter a Number"))
+print(num)
+nl=int(input("Enter marks 1:"))
+n2=int(input("Enter Marks 2:"))
+n3=int(input("Enter Marks 3:"))
+num=n1+n2+n3
+avg=num/3
+print("The Average of n1, n2, n3 is", avg)
