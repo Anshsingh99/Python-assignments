a6ba85c4440c299ed42006d8f0b42f837c042705
Anshsingh99 <137695101+Anshsingh99@users.noreply.github.com>
2023-06-28 20:04:24 +0530

    Add files via upload

---
+++ assingnment4.py
@@ -0,0 +1,28 @@
+a="ansh"
+# example of for with loop
+for i in a:
+    if i=='s':
+        continue #use of continue 
+    else:
+        print(i)
+else:
+    print("successfull")
+i=1
+# use of while with else loop
+while(i<=10):
+    if i==5:
+        i+=1
+        break # use of break
+    else:
+        print(i)
+    i+=1
+else:
+    print('its a break')
+j=1
+
+while(j<10):
+    if(j==7):
+        pass # use of pass
+    else:
+        j+=1
+
