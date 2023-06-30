1e9d709c34640a4e30b8fb03b4b83d661bf89a70
Anshsingh99 <137695101+Anshsingh99@users.noreply.github.com>
2023-06-30 22:00:47 +0530

    Update assingment6.py

--- assingment6.py
+++ assingment6.py
@@ -1,6 +1,7 @@
+
 def df(roll,name):
     li=[roll,name]
-    tu=(roll,name)
+    t1=(roll,name)
     se={roll,name}
     dic={
         "rollno":roll,
@@ -8,14 +9,29 @@ def df(roll,name):
     }
     print("Values Are Stored:- ")
     print(li)
-    print(tu)
+    print(t1)
     print(se)
     print(dic)
-print("Enter Rollno and Name to Store it:")
-r=input()
-n=input()
-df(r,n)
-print("Enter New Rollno and Name to Store it:") #HERE VALUES ARE CHANGING AT RUN TIME
-r=input()
-n=input()
-df(r,n)
+
+    print("Enter New Rollno and Name to Store it:") #HERE VALUES ARE CHANGING AT RUN TIME
+    r=input()
+    n=input()
+
+    li.append([r,n])
+    t2=(r,n)
+    t1=t1+t2
+    se.add((r,n))
+    d={
+        "rollno_new":roll,
+        "name_new":name
+    }
+    dic.update(d)
+    print("Values After Change:- ")
+    print(li)
+    print(t1)
+    print(se)
+    print(dic)
+
+df("21","ansh")
+
+
