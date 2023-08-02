3175eada3fd60444fe98a9e02c79b67f6d0642a9
Anshsingh99 <137695101+Anshsingh99@users.noreply.github.com>
2023-08-02 23:27:25 +0530

    Update assignment7.py

--- assignment7.py
+++ assignment7.py
@@ -1,9 +1,9 @@
-def fileHandle(file):
+def fileHandle(file="demo.txt"):
     try:
         f=open(file,"+a")
         f.writelines(["rollno=21 ","Name=Ansh ","Class=Tyif "])
         f.seek(0)
-        print(f.readline())
+        print(f.readlines())
     except IOError:
         print("File Operation Failed")
 
